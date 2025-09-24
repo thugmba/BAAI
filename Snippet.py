39872f34d11767bbcb20441bb69aef39b91ca84a
Brian <thugmba@gmail.com>
2025-09-24 15:49:31 +0800

    .

---
+++ Snippet.py
@@ -0,0 +1,19 @@
+{
+    "Task Template": {
+        "prefix": "jamestask",
+        "body": [
+            "#",
+            "# ${TM_FILENAME_BASE/(^[^_]*)_.*/$1/}, ${CURRENT_YEAR}/${CURRENT_MONTH}/${CURRENT_DATE}",
+            "# File: ${TM_FILENAME}",
+            "# Short description of the task",
+            "#",
+            "",
+            "# 1. Input",
+            "",
+            "# 2. Process",
+            "",
+            "# 3. Output"
+        ],
+    "description": "Insert James' task template"
+    }
+}
